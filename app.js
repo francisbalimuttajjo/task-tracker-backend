80d941ca40e1cc74405b0bb977572043fc355b2c
francis balimuttajjo <fbalimuttajjo@gmail.com>
2021-12-12 17:09:34 +0300

    added protection to the routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
 const cors = require("cors");
 const path = require("path");
 const TaskRouter = require("./routes/Task");
@@ -18,10 +22,21 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(mongoSanitize());
 app.use(cookieParser());
 
+app.use(helmet());
+app.use(express.json({ limit: "10kb" }));
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
 // Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
--- routes/User.js
+++ routes/User.js
@@ -3,7 +3,16 @@ const router = express.Router();
 const userController = require("../Controllers/User");
 const auth = require("../controllers/auth");
 
+router.route("/users/changePassword").post(auth.isAllowed, auth.updatePassword);
+router.route("/users/forgotPassword").post(userController.forgotPassword);
+router.route("/users/passwordReset/:token").post(userController.resetPassword);
+router.route("/users/login").post(auth.login);
+router.route("/users/auth").get(auth.isAuthenticated);
+router.route("/users/logout").get(auth.logOutHandler);
 router.route("/users/register").post(userController.register);
+router
+  .route("/users/activate-account/:token")
+  .get(userController.confirmAccount);
 router
   .route("/users/update")
   .post(
@@ -12,16 +21,4 @@ router
     userController.resizePhoto,
     userController.update
   );
-router.route("/users/changePassword").post(auth.isAllowed, auth.updatePassword);
-router.route("/users/forgotPassword").post(userController.forgotPassword);
-router.route("/users/passwordReset/:token").post(userController.resetPassword);
-router.route("/users/login").post(
-  // auth.isActive,
-  auth.login
-);
-router.route("/users/auth").get(auth.isAuthenticated);
-router.route("/users/logout").get(auth.logOutHandler);
-router
-  .route("/users/activate-account/:token")
-  .get(userController.confirmAccount);
 module.exports = router;
